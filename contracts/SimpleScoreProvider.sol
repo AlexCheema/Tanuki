d3e26732b29dec93d6205c0acd90e6603a48be83
Konrad Gnat <konradmgnat@gmail.com>
2023-04-16 01:15:05 +0900

    add next js and hardhat apps

--- contracts/SimpleScoreProvider.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.8.0 <0.9.0;
-
-import "./IScoreProvider.sol";
-
-contract SimpleScoreProvider is IScoreProvider {
-    function viewScore(address account) external view override returns (uint256) {
-        return 1;
-    }
-}
